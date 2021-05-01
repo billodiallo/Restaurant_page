56406ac22d8fd31b71b8eb58d5dec7f3f59f94e5
Billo Diallo <billodiallonet@gmail.com>
2021-05-01 20:29:43 +0300

    Add another module in webpack.js

--- main.js
+++ main.js
@@ -15,7 +15,7 @@
   \**********************/
 /***/ (() => {
 
-eval("\n\n//# sourceURL=webpack://restaurant_project/./src/index.js?");
+eval("console.log('hello')\n\n//# sourceURL=webpack://restaurant_project/./src/index.js?");
 
 /***/ })
 
