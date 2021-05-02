c6815ce75b7346454db2c413eb0acf3dbf740c61
Billo Diallo <billodiallonet@gmail.com>
2021-05-02 15:43:45 +0300

    Add container,classlist and attribute

---
+++ src/food.js
@@ -0,0 +1,15 @@
+import Thiakiri from './img/thiakiri.jpg';
+import Thiebouwekh from './img/thiebouwekh.jpg';
+import Thieboudienne from './img/thieboudienne';
+import Yassa  from './img/yassa.jpg';
+
+const foodpage = (() => {
+    const contentContainer = document.querySelector('#content');
+    const food = document.createElement('div');
+    food.classList.add('container-fluid');
+    food.setAttribute('id', 'menu');
+    food.setAttribute('data-tab-content', '');
+
+    
+
+})();
