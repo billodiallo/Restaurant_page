c6f8312763db3d3a1778847a1aecde9496c09530
Billo Diallo <billodiallonet@gmail.com>
2021-05-02 21:03:13 +0300

    Add navigation tabs

--- src/index.js
+++ src/index.js
@@ -1,13 +1,35 @@
 import './style.css';
 import headerPage from './header';
+import foodpage from './food';
 
 function fordbd() {
   const i = 0;
   if (i < 0) {
     headerPage();
-    // renderHome();
     foodpage();
+    renderHome();
     // contactPage();
   }
 }
 fordbd();
+
+// Navigation tabs
+tabs.forEach((tab) => tab.addEventListener('click', () => {
+  const target = document.querySelector(tab.dataset.tabTarget);
+  tabContents.forEach((tabContent) => {
+    tabContent.classList.remove('active');
+  });
+  tabs.forEach((tab) => {
+    tab.classList.remove('red');
+  });
+  tab.classList.add('red');
+  target.classList.add('active');
+}));
+
+document.querySelector('.main-row').addEventListener('click', () => {
+  document.querySelector('[data-tab-target="#menu"]').classList.add('red');
+});
+
+document.querySelector('[type="submit"]').addEventListener('click', () => {
+  document.querySelector('form').reset();
+});
