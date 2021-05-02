693c45e9fda7d4e30eeae21674a8f3f165c31ca4
Billo Diallo <billodiallonet@gmail.com>
2021-05-02 21:27:30 +0300

    Add elemet,attributes

---
+++ src/contact.js
@@ -0,0 +1,8 @@
+const contactPage = (() => {
+    const contentContainer = document.querySelector('#content');
+    const contact = document.createElement('div');
+    contact.classList.add('container-fluid');
+    contact.setAttribute('id', 'contact');
+    contact.setAttribute('data-tab-content', '');
+
+})();
