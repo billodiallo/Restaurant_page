c56ef0a4e8b2da177014294bd540e1134f7fc93e
Billo Diallo <billodiallonet@gmail.com>
2021-05-02 21:00:17 +0300

    Add home page

---
+++ src/home.js
@@ -0,0 +1,18 @@
+const renderHome = (() => {
+    //container  
+
+    const container = document.getElementById('content')
+    const home = document.getElementById('div');
+    home.classList.add('tab-content');
+    home.innerHTML = `
+    <div id="home" class="active" data-tab-content>
+    <div class='container position-relative'>
+      <div class='row col-6 mt-5 mx-auto main-row py-5'>
+        <h1 class='py-5 text-center'>Fast Food chez Joe</h1>
+        <h4 class='text-center'></h4>
+      </div>
+    </div>
+  </div>`;
+    container.appendChild(home);})();
+
+    export default renderHome;
